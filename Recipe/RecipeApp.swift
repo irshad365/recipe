4256ef031d67d6a3ec8d9c0a0f3070fbd7d39eef
Mohamed Irshad <irshad365@gmail.com>
2024-11-06 20:49:52 -0500

    Initial Commit

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by Mohamed Irshad on 11/6/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
